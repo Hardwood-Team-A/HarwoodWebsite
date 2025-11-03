29b1b6b2cf2c9373a8c0c300f6e7c8e5efc6126b
annasouk <anna.soukhovei@colorado.edu>
2025-11-02 21:15:46 -0700

    working on individual artist pages

--- Src/client/src/App.jsx
+++ Src/client/src/App.jsx
@@ -2,20 +2,21 @@ import { Link, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
+import Artists from './pages/Artists.jsx'
 
 function App() {
   return (
     <div>
       <nav>
-        <ul style={{ display: 'flex', gap: '1rem' }}>
+        <ul style={{ display: 'flex', gap: '1rem' }} id="navbar">
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+          <li><Link to="/artists">Artists</Link></li>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/artists" element={<Artists />} />
       </Routes>
     </div>
   )
---
+++ Src/client/src/pages/Artists.jsx
@@ -0,0 +1,7 @@
+export default function Artists() {
+  return (
+    <section>
+      <h1>Artists</h1>
+    </section>
+  )
+}
